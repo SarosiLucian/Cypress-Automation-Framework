de5a6b2e0fef2a001d271d7bb9a96e044f7573fd
SarosiLucian <sarosi.karoly@yahoo.com>
2023-05-26 13:40:57 +0300

    Small changes

--- cypress/e2e/webdriver-uni/checkboxes.cy.js
+++ cypress/e2e/webdriver-uni/checkboxes.cy.js
@@ -6,20 +6,15 @@ describe("Verify checkboxes via webdriveruni", () =>{
     })
 
     it("Check and validate checkbox", () => {
-        //cy.get('#checkboxes > :nth-child(1) > input').check()
-        // cy.get('#checkboxes > :nth-child(1) > input').check().should('be.checked')
 
-        cy.get('#checkboxes > :nth-child(1) > input').as('option-1')
-        //cy.get('@option-1').check()  
+        cy.get('#checkboxes > :nth-child(1) > input').as('option-1') 
         cy.get('@option-1').check().should('be.checked')
 
     });
 
     it("Uncheck and validate checkbox", () => {
         cy.get(':nth-child(5) > input').as('option-3')
-        //cy.get('@option-3').uncheck()
         cy.get('@option-3').uncheck().should('not.be.checked')
-
 
     });
 
